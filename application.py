caba1a4610ec8d09a11a19a97b914df9cb2e6fa1
Inzamam Baig <baigsone@gmail.com>
2021-09-22 23:46:18 +0500

    Modified Requirements.txt

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ from app import app, db
 
 # Run Server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- config.py
+++ config.py
@@ -1,11 +1,13 @@
+
 class Config:
-    ENV = 'dev'
+    ENV = 'prod'
     if ENV == 'dev':
         user = 'postgres'
         pwd = 'startadventure'
         port = 5432
         SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{pwd}@localhost:{port}/getadventure'.format(user=user, pwd=pwd, port=port)
     else:
-        SQLALCHEMY_DATABASE_URI = ''
+        SQLALCHEMY_DATABASE_URI = 'postgres://usulnouomwtgla:3bc225afca6eb152cb470cafa5200b08b49127f6fbd4c378a3199744225b2a45@ec2-54-209-52-160.compute-1.amazonaws.com:5432/d1ltf69ig2melb'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
