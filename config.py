d8751545e6150277ec8d6c9cb8afead985fe69f8
Inzamam Baig <baigsone@gmail.com>
2021-09-23 21:30:23 +0500

    Updated App

--- config.py
+++ config.py
@@ -2,10 +2,11 @@
 class Config:
     ENV = 'prod'
     if ENV == 'dev':
-        user = 'postgres'
-        pwd = 'startadventure'
-        port = 5432
-        SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{pwd}@localhost:{port}/getadventure'.format(user=user, pwd=pwd, port=port)
+        # user = 'postgres'
+        # pwd = 'startadventure'
+        # port = 5432
+        # SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{pwd}@localhost:{port}/getadventure'.format(user=user, pwd=pwd, port=port)
+        pass
     else:
         SQLALCHEMY_DATABASE_URI = 'postgresql://dvsryfmfnohvjy:d44c0493ff026c017e1d6d1967f593e8d41cd95edb3d26bd2dfb5175a6ed82c9@ec2-44-199-83-229.compute-1.amazonaws.com:5432/d5q9uqdl9ak143'
 
