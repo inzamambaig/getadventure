0d11db00cc738aa3c780a9a6fb61e7d1d6254062
Inzamam Baig <baigsone@gmail.com>
2021-09-23 20:47:40 +0500

    Updated Modules

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 
 class Config:
-    ENV = 'dev'
+    ENV = 'prod'
     if ENV == 'dev':
         user = 'postgres'
         pwd = 'startadventure'
         port = 5432
         SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{pwd}@localhost:{port}/getadventure'.format(user=user, pwd=pwd, port=port)
     else:
-        SQLALCHEMY_DATABASE_URI = 'postgres://usulnouomwtgla:3bc225afca6eb152cb470cafa5200b08b49127f6fbd4c378a3199744225b2a45@ec2-54-209-52-160.compute-1.amazonaws.com:5432/d1ltf69ig2melb'
+        SQLALCHEMY_DATABASE_URI = 'postgres://dvsryfmfnohvjy:d44c0493ff026c017e1d6d1967f593e8d41cd95edb3d26bd2dfb5175a6ed82c9@ec2-44-199-83-229.compute-1.amazonaws.com:5432/d5q9uqdl9ak143'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
