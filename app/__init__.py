e501a8a8e067793155ff55c3c5c0a837b8e415f8
Inzamam Baig <baigsone@gmail.com>
2021-10-06 02:32:52 +0500

    Refactored API

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from config import Config
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 KEY = '\x8f\xe9\xf7b\xfd\x0f\xd3\xcdiJ4\x1aH\xae\xebw\xa6%r.\xca\x12jM!\xdfc\xdcf\x92h5\xec}\xb3tgcW\xbe\xd2\x82~W\x83\nBE\x9d\xfeq]\x11\xce"\xc3d9\x10\xfb1cp7'  # constant key declaration
